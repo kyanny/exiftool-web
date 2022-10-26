a5b9a53fa5fd6142a637d89ba9e672b8a07661c1
Kensuke Nagae <kyanny@github.com>
2022-10-27 00:57:05 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+set -x
+
 bundle install
 
-curl -s https://fastapi.metacpan.org/v1/download_url/Image::ExifTool > Image-ExifTool.json
+curl https://fastapi.metacpan.org/v1/download_url/Image::ExifTool > Image-ExifTool.json
 download_url=$(ruby -r json -e 'puts JSON.parse($stdin.read)["download_url"]' < Image-ExifTool.json)
 archive=$(basename "$download_url")
 directory=$(basename "$download_url" .tar.gz)
-echo ==========
 if [ ! -f "$archive" ]
 then
     curl -O "$download_url"
